69f35dbd32d5dec41026f8b45bc6baafb56a058a
unique_hunter <77807213+oguz162@users.noreply.github.com>
2021-04-13 12:19:09 +0300

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -247,3 +247,13 @@ Asena.addCommand({ pattern: 'textttp ?(.*)', fromMe: true, desc: Lang.HARRY_DESC
 
 }));
 
+Asena.addCommand({ pattern: 'silverttp ?(.*)', fromMe: true, desc: Lang.HARRY_DESC }, (async (message, match) => {
+
+    if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
+
+    var ttinullimage = await axios.get(`https://api.xteam.xyz/textpro/deluxesilver?text=${match[1].replace(/Ö/g, "%C3%96").replace(/ö/g, "%C3%B6").replace(/ü/g, "%C3%BC").replace(/Ü/g, "%C3%9C").replace(/Ğ/g, "%C4%9E").replace(/ğ/g, "%C4%9F").replace(/ş/g, "%C5%9F").replace(/Ş/g, "%C5%9E").replace(/ç/g, "%C3%A7").replace(/Ç/g, "%C3%87").replace(/ı/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/İ/g, "%C4%B0")}&APIKEY=e67bd1bafe81b611`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'SELO 💑 ZEYNO' })
+
+}));
+
